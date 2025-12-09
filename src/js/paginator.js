79b96c9cb6312dba54d37fc18a55385138e74f53
Patrick <patrick_schauer@t-online.de>
2025-12-09 11:56:07 +0100

    Refactor: Modularization (14 loc) of pagination component finished

--- src/js/paginator.js
+++ src/js/paginator.js
@@ -66,11 +66,10 @@ export const computeWindow = (page) => {
  * Renders left edge for pagination.
  * @loc 3
  * @function renderLeftEdge
- * @param {CallableFunction} add - Callable that inherits createPageItem(label, value, disable, active)
+ * @param {CallableFunction} add - Callable that inherits createPageItem (label, value, disable, active)
  * @returns {json} {start: start, end: end} - JSON that contains start and end.
  */
 export const renderLeftEdge = (add, page, start) => {
-    // args: label, value, disable, active; active defaults to false
     add('<i class="material-icons">chevron_left</i>', Math.max(1, page - 1), page === 1);
 
     add(1, 1, false, page === 1); // 1
@@ -81,11 +80,10 @@ export const renderLeftEdge = (add, page, start) => {
 /**
  * Renders carousel for pagination.
  * @loc 1
- * @param {CallableFunction} add - Callable that inherits createPageItem(label, value, disable, active)
+ * @param {CallableFunction} add - Callable that inherits createPageItem (label, value, disable, active)
  * @returns {json} {start: start, end: end} - JSON that contains start and end.
  */
 export const renderWindow = (add, page, start, end) => {
-    // args: label, value, disable, active
     for (let i = start; i <= end; i++) add(i, i, false, i === page);
 };
 
@@ -93,13 +91,12 @@ export const renderWindow = (add, page, start, end) => {
 /**
  * Renders carousel for pagination.
  * @loc 3
- * @param {CallableFunction} add - Callable that inherits createPageItem(label, value, disable, active)
+ * @param {CallableFunction} add - Callable that inherits createPageItem (label, value, disable, active)
  * @returns {json} {start: start, end: end} - JSON that contains start and end.
  */
 export const renderRightEdge = (add, page, start, end) => {
-    // args: label, value, disable, active; end defaults to false
-    if (end < TOTAL_PAGES - 1) add("…", null, true); // ...
+    if (end < TOTAL_PAGES - 1) add("…", null, true);
 
-    add(TOTAL_PAGES, TOTAL_PAGES, false, page === TOTAL_PAGES); // 35
+    add(TOTAL_PAGES, TOTAL_PAGES, false, page === TOTAL_PAGES);
     add('<i class="material-icons">chevron_right</i>', Math.min(TOTAL_PAGES, page + 1), page === TOTAL_PAGES);
 };
