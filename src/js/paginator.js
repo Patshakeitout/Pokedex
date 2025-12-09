0f1d185fba1c149d6523a1afc5948b3118634c4a
Patrick <patrick_schauer@t-online.de>
2025-12-09 11:55:52 +0100

    Refactor: Modularization (14 loc) of pagination component finished

--- src/js/paginator.js
+++ src/js/paginator.js
@@ -1,13 +1,17 @@
-// paginator.js
+/**
+ * @file paginator.js
+ * @fileoverview Module for the paginator component.
+ */
 
-// PAGINATION
 export const PAGE_CARDS = 20;
 export const TOTAL_PAGES = Math.ceil(1025 / PAGE_CARDS);
 const INNER_CIRCLE = 7;
 const OFFSET = Math.floor(INNER_CIRCLE / 2);
 
+
 /**
  * Creates a single list item element for pagination (li).
+ * @function createPageItem
  * @loc 9
  * @param {string|number} label - The text or HTML content for the link.
  * @param {number} page - The target page number for the data attribute.
@@ -32,6 +36,7 @@ export const createPageItem = (label, page, disabled = false, active = false) =>
 /**
  * Computes the inner circle window for pagination.
  * @loc 14
+ * @function computeWindow
  * @param {number} page - The target page number for the data attribute.
  * @returns {json} {start: start, end: end} - JSON that contains start and end.
  */
@@ -39,20 +44,17 @@ export const computeWindow = (page) => {
     let start = Math.max(2, page - OFFSET);
     let end = Math.min(TOTAL_PAGES - 1, page + OFFSET);
 
-    // Edge case: if page is at the very beginning
     if (page <= INNER_CIRCLE + 1 - OFFSET) {
-        end = Math.min(TOTAL_PAGES - 1, INNER_CIRCLE + 1); // grow to 10
+        end = Math.min(TOTAL_PAGES - 1, INNER_CIRCLE + 1);
     }
-
-    // Edge case: start too small
+    
     if (start < 2) {
         end = Math.min(TOTAL_PAGES - 1, INNER_CIRCLE);
         start = 2;
     }
-
-    // Edge case: end too large
+    
     if (page >= TOTAL_PAGES - INNER_CIRCLE + OFFSET) {
-        start = Math.max(2, TOTAL_PAGES - INNER_CIRCLE);   // grow backward
+        start = Math.max(2, TOTAL_PAGES - INNER_CIRCLE);
         end = TOTAL_PAGES - 1;
     }
 
@@ -63,6 +65,7 @@ export const computeWindow = (page) => {
 /**
  * Renders left edge for pagination.
  * @loc 3
+ * @function renderLeftEdge
  * @param {CallableFunction} add - Callable that inherits createPageItem(label, value, disable, active)
  * @returns {json} {start: start, end: end} - JSON that contains start and end.
  */
