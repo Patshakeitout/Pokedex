5c619d587424f2194311cb8344e0238748dff5bf
Patrick <patrick_schauer@t-online.de>
2025-12-13 16:39:14 +0100

    Feature: Correction to header and link to DevAka landing page

--- src/js/utils/helpers.js
+++ src/js/utils/helpers.js
@@ -7,16 +7,16 @@ export const renderSpinner = () => {
   let cardGrid = document.getElementById('cardGrid');
   cardGrid.innerHTML = ''; 
   cardGrid.innerHTML = `<div class="preloader-wrapper big active">
-                      <div class="spinner-layer spinner-blue-only">
-                          <div class="circle-clipper left">
-                              <div class="circle"></div>
-                          </div><div class="gap-patch">
-                              <div class="circle"></div>
-                          </div><div class="circle-clipper right">
-                              <div class="circle"></div>
+                          <div class="spinner-layer spinner-blue-only">
+                              <div class="circle-clipper left">
+                                  <div class="circle"></div>
+                              </div><div class="gap-patch">
+                                  <div class="circle"></div>
+                              </div><div class="circle-clipper right">
+                                  <div class="circle"></div>
+                              </div>
                           </div>
-                      </div>
-            </div>`;
+                        </div>`;
 }
 
 
